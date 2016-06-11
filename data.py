78845f6a019f8a6f650d4ef36c9746e50ad857fb
Nadine Dietmann <nadinedietmann@gmail.com>
2016-06-11 21:25:04 +0100

    data loaded

---
+++ data.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import os
+
+town_state = pd.read_csv('raw/town_state.csv')
+#products = pd.read_csv('raw/producto_tabla.csv')
+#clients = pd.read_csv('raw/cliente_tabla.csv')
+#test = pd.read_csv('raw/test.csv')
+#train = pd.read_csv('raw/train.csv')
+#sample_submission = pd.read_csv('raw/sample_submission.csv')
--- load/load.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import os
-
-town_state = pd.read_csv('data/town_state.csv')
-products = pd.read_csv('data/producto_tabla.csv')
-clients = pd.read_csv('data/cliente_tabla.csv')
-test = pd.read_csv('data/test.csv')
-train = pd.read_csv('/data/train.csv')
-sample_submission = pd.read_csv('data/sample_submission.csv')
