1b15ae5f88ff164dc2ac80fb5126b22823d9f75b
phildeutsch <philipp.g.deutsch@gmail.com>
2016-06-12 11:08:49 +0100

    first prediction

--- data.py
+++ data.py
@@ -1,9 +1,15 @@
 import pandas as pd
 import os
+from config import *
 
-town_state = pd.read_csv('raw/town_state.csv')
-#products = pd.read_csv('raw/producto_tabla.csv')
-#clients = pd.read_csv('raw/cliente_tabla.csv')
-#test = pd.read_csv('raw/test.csv')
-#train = pd.read_csv('raw/train.csv')
-#sample_submission = pd.read_csv('raw/sample_submission.csv')
+try:
+    NROWS
+except NameError:
+    NROWS = None
+
+town_state = pd.read_csv('raw/town_state.csv', nrows=NROWS)
+products = pd.read_csv('raw/producto_tabla.csv', nrows=NROWS)
+clients = pd.read_csv('raw/cliente_tabla.csv', nrows=NROWS)
+test = pd.read_csv('raw/test.csv', nrows=NROWS)
+train = pd.read_csv('raw/train.csv', nrows=NROWS)
+sample_submission = pd.read_csv('raw/sample_submission.csv', nrows=NROWS)
---
+++ run.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.linear_model import SGDClassifier
+
+# Get data
+import data
+
+# Select variables to use
+common_names = list(set(data.train.columns.values) & set(data.test.columns.values))
+common_names.remove('Semana')
+
+X_train = data.train[common_names]
+y_train = data.train['Demanda_uni_equil']
+X_test = data.test[common_names]
+
+# Fit SGD classifier
+clf = SGDClassifier(loss="hinge", penalty="l2")
+clf.fit(X_train, y_train)
+
+# Get results
+y = pd.DataFrame({'Demanda_uni_equil' : clf.predict(X_test)})
+submission = pd.concat([data.test['id'], y], axis=1)
+submission.to_csv('submission.csv', index=False)
