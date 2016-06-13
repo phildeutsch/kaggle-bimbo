20822dabcf8e782e5be2cfbbcc0c4fdcabade331
Philipp Deutsch <philipp.deutsch@occstrategy.com>
2016-06-13 16:56:07 +0100

    Clean up loading script

--- R/01_load.R
+++ R/01_load.R
@@ -1,13 +1,49 @@
 require(data.table)
+require(stringr)
 
-# Load csv files
+# Options
+SAVE_PATH = 'R/data/'
+LOAD_PATH = 'raw/'
+LOAD_FROM_IMAGE = TRUE
 
-clients = fread('raw/cliente_tabla.csv')
-products = fread('raw/producto_tabla.csv')
-town_state = fread('raw/town_state.csv')
-test = fread('raw/test.csv')
-train = fread('raw/train.csv')
+###############################################################################
 
-# Get only the first client ID for each client
-clients = clients %>% group_by(Cliente_ID) %>% summarize(NombreCliente = min(NombreCliente))
+time_start = Sys.time()
+if (LOAD_FROM_IMAGE) {
+  cat(str_c(Sys.time(), " Loading images..."))
+  clients = readRDS(str_c(SAVE_PATH, "clients.rds"))
+  products = readRDS(str_c(SAVE_PATH, "products.rds"))
+  town_state = readRDS(str_c(SAVE_PATH, "town_state.rds"))
+  test = readRDS(str_c(SAVE_PATH, "test.rds"))
+  train = readRDS(str_c(SAVE_PATH, "train.rds"))
+  cat("done.\n")
+} else {
+  # Load csv files
+  cat(str_c(Sys.time(), " Loading csv files..."))
+  clients = fread('raw/cliente_tabla.csv')
+  products = fread('raw/producto_tabla.csv')
+  town_state = fread('raw/town_state.csv')
+  test = fread('raw/test.csv')
+  train = fread('raw/train.csv')
+  cat("done.\n")
+  
+  # Get only the first client ID for each client
+  clients = clients %>% group_by(Cliente_ID) %>% summarize(NombreCliente = min(NombreCliente))
+  
+  # Save to rds files
+  saveRDS(clients, str_c(SAVE_PATH, "clients.rds"), compress=FALSE)
+  saveRDS(products, str_c(SAVE_PATH, "products.rds"), compress=FALSE)
+  saveRDS(town_state, str_c(SAVE_PATH, "town_state.rds"), compress=FALSE)
+  saveRDS(test, str_c(SAVE_PATH, "test.rds"), compress=FALSE)
+  saveRDS(train, str_c(SAVE_PATH, "train.rds"), compress=FALSE)
+}
 
+# Clean up
+cat(str_c(Sys.time(), " Clean up..."))
+rm(list=intersect(ls(), c("clients", "products", "town_state", "test", "train")))
+gc()
+cat("done.\n")
+
+time_end = Sys.time()
+cat(str_c("Total time: ", round(difftime(time_end, time_start, units = "mins"),1), " mins.\n"))
+    
