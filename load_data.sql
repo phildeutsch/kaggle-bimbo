93398d630bdfeb0ba7ffdf256bae70b2f1779a6d
phildeutsch <philipp.g.deutsch@gmail.com>
2016-07-06 00:10:02 +0100

    start loading data to postgres

---
+++ load_data.sql
@@ -0,0 +1,8 @@
+drop table if exists town_state;
+
+create table town_state (
+  store_id text,
+  town text,
+  state text);
+
+\copy town_state from 'raw/town_state.csv' with (format csv, delimiter ',')
